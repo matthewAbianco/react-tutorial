9a4a50da2e5ac1a8cebb1ea3069c0666b3037efe
matthewAbianco <matthewbianco@hotmail.com>
2022-03-09 11:55:35 -0500

    cleaned up code

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,22 +9,20 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
 
     return (
-        <div className='navbar'>
-            <div className='navbar__container'>
-                <div className='container__content'>
-                    <div className='content__title'>
-                        {/* target __blank opens the link in a new tab */}
-                        <a href='https://www.w3schools.com/react/default.asp' target='__blank' >
-                            <FaReact className='content__title--icon' />
-                            <h2>React</h2>
-                        </a>
-                    </div>
-                    <div className='content content__tutorial' >
-                        <Link to='/'><h2>React Tutorial</h2></Link>
-                    </div>
-                    <div className='content content__hooks'>
-                        <Link to='/whatIsAHook'><h2>React Hooks</h2></Link>
-                    </div>
+        <div className='navbar__container'>
+            <div className='navbar__content'>
+                <div className='navbar__title'>
+                    {/* target __blank opens the link in a new tab */}
+                    <a href='https://www.w3schools.com/react/default.asp' target='__blank' >
+                        <FaReact className='content__title--icon' />
+                        <h2>React</h2>
+                    </a>
+                </div>
+                <div className='navbar__content content__tutorial' >
+                    <Link to='/'><h2>React Tutorial</h2></Link>
+                </div>
+                <div className='navbar__content content__hooks'>
+                    <Link to='/whatIsAHook'><h2>React Hooks</h2></Link>
                 </div>
             </div>
         </div>
