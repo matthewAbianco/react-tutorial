943c900b4af798ccdd824c54ea196ee83c0ba28d
matthewAbianco <matthewbianco@hotmail.com>
2022-03-10 16:34:14 -0500

    finished setting up the page and generally happy with styling. but touch ups will be needed down the line

--- src/components/reactTutorial/tutorialOptions/getStarted/GetStarted.js
+++ src/components/reactTutorial/tutorialOptions/getStarted/GetStarted.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import './GetStarted.css'
-import { LoremIpsum } from 'react-lorem-ipsum'
+import first from './assets/first.png'
+import second from './assets/second.png'
+import third from './assets/third.png'
+import fourth from './assets/fourth.png'
+
+// const Picture = styled.picture`
+
+// `
 
 const GetStarted = () => {
     return (
@@ -9,17 +16,55 @@ const GetStarted = () => {
 
                 <div className='started__section started__title'>
                     <h2>Starting React</h2>
-                    <p> <LoremIpsum p={1} /> </p>
+                    <div className='started__title__container'>
+                        <h3> * Requirement * </h3>
+                        <p>To use React in production, you need npm (Node Package Manager) which is included with <a href='https://nodejs.org/en/' className='started__link'>Node.js</a></p>
+                    </div>
+                    <div className='started__title__container'>
+                        <h3> How to install using npm </h3>
+                        <ul>
+                            <li>npx create-react-app app-name-here</li>
+                            <li>npm init react-app app-name-here</li>
+                            <li>yarn create react-app app-name-here</li>
+                        </ul>
+                    </div>
+                    <div className='started__title__container'>
+
+                    </div>
                 </div>
 
                 <div className='started__section started__react__html'>
                     <h2>React In Html</h2>
-                    <p> <LoremIpsum p={1} /> </p>
+                    <p> While React can be brought into an html document. That is outside the scope of this tutoriual.  </p>
                 </div>
 
                 <div className='started__section started__react__setup'>
-                    <h2>Setting up and running react</h2>
-                    <p> <LoremIpsum p={1} /> </p>
+                    <div className='started__image__container'>
+                        <h2>Setting up and running react</h2>
+                        <h4>This is how to start react in your integrated terminal. While any of the above three ways work the same way. I will be using Yarn for this example.</h4>
+                    </div>
+                    <div className='started__photos__container'>
+                        <div className='started__image__container'>
+                            <h3>Step One</h3>
+                            <p>Type "yarn create react-app" then type the name of your project. </p>
+                            <img className='started__image' src={first} alt='step one' />
+                        </div>
+                        <div className='started__image__container'>
+                            <h3>Step Two</h3>
+                            <p>Type "cd app-name-here" once the install has completed to get into the react application </p>
+                            <img className='started__image' src={second} alt='step two' />
+                        </div>
+                        <div className='started__image__container'>
+                            <h3>Step Three</h3>
+                            <p> Once inside your app. You will be in the "Master" branch. To get React open on Local Host, type "yarn start"</p>
+                            <img className='started__image' src={third} alt='step three' />
+                        </div>
+                        <div className='started__image__container'>
+                            <h3>Step Four</h3>
+                            <p>Once React has successfully opened. This window will automatically open in your browser. Congratulations! You have now successfully started react!</p>
+                            <img className='started__image' src={fourth} alt='step four' />
+                        </div>
+                    </div>
                 </div>
 
             </div>
--- src/components/reactTutorial/tutorialOptions/home/Home.js
+++ src/components/reactTutorial/tutorialOptions/home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Home.css'
 import hi from './hi.png'
-import { LoremIpsum } from 'react-lorem-ipsum'
 
 const Home = () => {
     return (
@@ -20,12 +19,12 @@ const Home = () => {
                 </div>
 
                 <div className='home__about__section'>
-                    <p className='home__about__section__text'> <img className='home__hi__img' src={hi} alt='hi' /> This is a React app that I made so I could teach myself React. I built it based on the
-                        <a href='https://www.w3schools.com/react/react_getstarted.asp' className='home__about__section--anchor'>W3schools</a> layout.
+                    <p className='home__about__section__text'> <img className='home__hi__img' src={hi} alt='hi' /> This is a React app
+                        that I made so I could teach myself React. I built it based on the <a href='https://www.w3schools.com/react/react_getstarted.asp' className='home__about__section--anchor'>W3schools</a> layout.
                         While w3schools will be my primary source for my descriptions, Everything will be explained in my own words with links
                         given if neccessicary The CSS Styling will be inconsistent as I am looking to try new things and this is as good of a
                         medium as any. As well, having a visual reminder of the styling helps me remember it for future projects.
-                        <LoremIpsum p={2} />
+
                     </p>
                 </div>
             </div>
