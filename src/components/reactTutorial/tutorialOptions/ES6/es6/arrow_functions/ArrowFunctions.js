907d3fa22a36fbc8949e7d37d9a9c9ec7785bf6e
matthewAbianco <matthewbianco@hotmail.com>
2022-03-18 19:24:09 -0400

    FC

--- src/components/reactTutorial/tutorialOptions/ES6/es6/arrow_functions/ArrowFunctions.js
+++ src/components/reactTutorial/tutorialOptions/ES6/es6/arrow_functions/ArrowFunctions.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import './ArrowFunctions.css'
+import { LoremIpsum } from 'react-lorem-ipsum'
 
 const ArrowFunctions = () => {
     return (
-        <div>ArrowFunctions</div>
+        <div className='container'>
+            <div className='content'>
+                <div className='af__section'>
+                    <h2>Arrow Functions</h2>
+                    <p> Arrow functions reduce the length of a function syntax</p>
+                </div>
+                <div className='af__section'>
+                    <h2>Before</h2>
+                </div>
+                <div className='af__Section'>
+                    <h2>With Arrow Functions</h2>
+                </div>
+                <div className='af__Section'>
+                    <h2>Arrow Functions returning a value by default </h2>
+                </div>
+                <div className='af__Section'>
+                    <h2>Arrow Function with parameters</h2>
+                </div>
+                <div className='af__Section'>
+                    <h2>Arrow Function without parameters</h2>
+                </div>
+                <div className='af__Section'>
+                    <h2>Keyword "this"</h2>
+                    <LoremIpsum p={1} />
+                </div>
+                <div className='af__Section'>
+                    <h2>Arrow Function with parameters</h2>
+                </div>
+            </div>
+        </div>
     )
 }
 
