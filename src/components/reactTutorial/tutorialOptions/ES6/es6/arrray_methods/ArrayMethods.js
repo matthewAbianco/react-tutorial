4f9d3e75d4c0ca4e378931c787d113e748066019
matthewAbianco <matthewbianco@hotmail.com>
2022-03-22 19:58:12 -0400

    added array method assets

--- src/components/reactTutorial/tutorialOptions/ES6/es6/arrray_methods/ArrayMethods.js
+++ src/components/reactTutorial/tutorialOptions/ES6/es6/arrray_methods/ArrayMethods.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './ArrayMethods.css'
+import mapCode from './assets/mapCode.png'
+import mapOutcome from './assets/mapOutcome.png'
 
 const ArrayMethods = () => {
     return (
@@ -8,9 +10,25 @@ const ArrayMethods = () => {
                 <div className='am__section'>
                     <h1>Array Methods</h1>
                     <p>Javascript has an assortment of array methods, Which can be found here
-                        <a href='https://www.w3schools.com/js/js_array_methods.asp' target='__blank' className='home__about__section--anchor'> https://www.w3schools.com/js/js_array_methods.asp</a></p>
+                        <a href='https://www.w3schools.com/js/js_array_methods.asp' target='__blank'
+                            className='home__about__section--anchor'> https://www.w3schools.com/js/js_array_methods.asp</a>.
+
+                        But since we are discussing the new array methods with ES6, they are outside the scope of this lesson.
+                    </p>
+
+                    <p>For ES6, the new method introduced is called the "map()" array method. </p>
+                    <p> This method runs a function on each item in the array, then returning a new result</p>
+                    <p>This method is primarily used for creating lists</p>
                 </div>
                 <div className='am__section'>
+                    <div>
+                        <h3>map() code</h3>
+                        <img src={mapCode} alt='map code' />
+                    </div>
+                    <div>
+                        <h3> map outcome</h3>
+                        <img src={mapOutcome} alt='map code' />
+                    </div>
 
                 </div>
             </div>
