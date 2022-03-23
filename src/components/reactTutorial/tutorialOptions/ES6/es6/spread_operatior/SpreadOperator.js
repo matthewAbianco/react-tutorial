af2572e181a6f83c5867f6739c189c3b37ba57a8
matthewAbianco <matthewbianco@hotmail.com>
2022-03-23 13:52:25 -0400

    fc

--- src/components/reactTutorial/tutorialOptions/ES6/es6/spread_operatior/SpreadOperator.js
+++ src/components/reactTutorial/tutorialOptions/ES6/es6/spread_operatior/SpreadOperator.js
@@ -3,7 +3,14 @@ import './SpreadOperator.css'
 
 const SpreadOperator = () => {
     return (
-        <div>SpreadOperator</div>
+        <div className='spread__contaner'>
+            <div className='spread__content'>
+                <div className='spread__section'>
+                    <h1>Spread Operator</h1>
+                    <p>The spread operator is used to combine different objects and arrays into a single array. </p>
+                </div>
+            </div>
+        </div>
     )
 }
 
