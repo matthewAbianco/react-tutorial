9eac8c2e7739a600f1abff4e725cc23e1812ba7d
matthewAbianco <matthewbianco@hotmail.com>
2022-03-23 17:38:38 -0400

    finished adding assets, needs css

--- src/components/reactTutorial/tutorialOptions/ES6/es6/spread_operatior/SpreadOperator.js
+++ src/components/reactTutorial/tutorialOptions/ES6/es6/spread_operatior/SpreadOperator.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './SpreadOperator.css'
+import spreadCode from './assets/spreadCode.png'
+import spreadOutcome from './assets/spreadOutcome.png'
+
 
 const SpreadOperator = () => {
     return (
@@ -7,7 +10,18 @@ const SpreadOperator = () => {
             <div className='spread__content'>
                 <div className='spread__section'>
                     <h1>Spread Operator</h1>
-                    <p>The spread operator is used to combine different objects and arrays into a single array. </p>
+                    <p>The spread operator is used to combine different objects and arrays into a single objects or arrays. </p>
+                    <p> *Disclaimer* Spread children are not supported in React. Meaning you cannot destructure a '...insertConstNameHere' into a h2 or other tag. but the outcome can be seen in console.</p>
+                </div>
+                <div className='spread__section spread__images'>
+                    <div className='spread__image'>
+                        <h2>Spread Code</h2>
+                        <img src={spreadCode} alt='spread code' />
+                    </div>
+                    <div className='spread__image'>
+                        <h2>Spread Code</h2>
+                        <img src={spreadOutcome} alt='spread Outcome' />
+                    </div>
                 </div>
             </div>
         </div>
