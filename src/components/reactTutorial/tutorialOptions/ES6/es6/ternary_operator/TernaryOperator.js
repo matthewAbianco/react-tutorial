e739781861d76aadf1e633a6d47ab67dbbb907bc
matthewAbianco <matthewbianco@hotmail.com>
2022-03-24 16:15:07 -0400

    finished adding assets, Needs css

--- src/components/reactTutorial/tutorialOptions/ES6/es6/ternary_operator/TernaryOperator.js
+++ src/components/reactTutorial/tutorialOptions/ES6/es6/ternary_operator/TernaryOperator.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './TernaryOperator.css'
+import ifElse from './assets/ifElse.png'
+import ternary from './assets/ternary.png'
 
 const TernaryOperator = () => {
     return (
-        <div>TernaryOperator</div>
+        <div className='ternary__container'>
+            <div className='ternary__content'>
+                <div className='ternary__section'>
+                    <h1>Ternary Operator</h1>
+                    <p>The ternary operator simplifies the syntax of a conditional operator like "if/else" statements</p>
+                    <p>Ternary Operator Syntax: condition ? if condition true : if condition false</p>
+                </div>
+                <div className='ternary__section'>
+                    <h3>Here is the old syntax for an "if/else" statement</h3>
+                    <img src={ifElse} alt="" />
+                </div>
+                <div className='ternary__section'>
+                    <h3>Here is the old syntax for an "if/else" statement</h3>
+                    <img src={ternary} alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
 
