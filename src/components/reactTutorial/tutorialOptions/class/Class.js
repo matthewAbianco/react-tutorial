64a111a7a212500a0cefbfedb46534d90f3b7690
matthewAbianco <matthewbianco@hotmail.com>
2022-03-31 01:14:34 -0400

    added assets

--- src/components/reactTutorial/tutorialOptions/class/Class.js
+++ src/components/reactTutorial/tutorialOptions/class/Class.js
@@ -3,7 +3,16 @@ import './Class.css'
 
 const Class = () => {
     return (
-        <div>Class</div>
+        <div className='class__contaner'>
+            <div className='class__content'>
+                <div className='class__section'>
+                    <h1>React Class</h1>
+                    <p>Class based components are the previous method used for writing components.
+
+                        But ever since React version 16.8. Functional components became the primary method for writing components in react</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
