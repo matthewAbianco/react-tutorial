f6afb711751b77dfc835e6e7f6732d6263c5ff25
matthewAbianco <matthewbianco@hotmail.com>
2022-05-18 08:04:53 -0400

    finished styling destructuring page

--- src/components/reactTutorial/tutorialOptions/ES6/es6/destructuring/Destructuring.js
+++ src/components/reactTutorial/tutorialOptions/ES6/es6/destructuring/Destructuring.js
@@ -13,16 +13,17 @@ const Destructuring = () => {
                     <p>Destructuring is used to pull specific data from an array.</p>
                 </div>
                 <div className='dest__section'>
-
                     <h2> Destructuring Objects</h2>
-                    <p> Here is how you destrucre an object</p>
+                    <p>Below is how you destrucre an object</p>
+                </div>
+                <div className='dest__section'>
 
                     <div className='dest__image'>
-                        <h3> Destructing in functional component </h3>
+                        <h2> Destructing in functional component </h2>
                         <img src={destructuringCode} alt='destructured code' />
                     </div>
                     <div className='dest__image'>
-                        <h3> Code Outcome </h3>
+                        <h2> Code Outcome </h2>
                         <img src={destructuringOutcome} alt='destructured code outcome' />
                     </div>
                 </div>
