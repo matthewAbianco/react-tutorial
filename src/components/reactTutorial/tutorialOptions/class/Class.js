b857183688d8a03a2769170ebfdadce55250ec25
matthewAbianco <matthewbianco@hotmail.com>
2022-05-20 14:06:14 -0400

    finished styling class page

--- src/components/reactTutorial/tutorialOptions/class/Class.js
+++ src/components/reactTutorial/tutorialOptions/class/Class.js
@@ -3,13 +3,16 @@ import './Class.css'
 
 const Class = () => {
     return (
-        <div className='class__contaner'>
+        <div className='class__container'>
             <div className='class__content'>
                 <div className='class__section'>
                     <h1>React Class</h1>
-                    <p>Class based components are the previous method used for writing components.
+                    <p>Class based components are the previous method used for writing components in React. </p>
 
-                        But ever since React version 16.8. Functional components became the primary method for writing components in react</p>
+                    <p>But ever since React version 16.8. Functional components became the primary method for writing components in React.</p>
+                    <p>Class based components became obsolete 4 years ago, so they are outside the scope of this lesson.</p>
+
+
                 </div>
             </div>
         </div>
