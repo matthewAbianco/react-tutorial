187ba06e00fb12d16927245c0fc7d8de71e44cf4
matthewAbianco <matthewbianco@hotmail.com>
2022-05-24 21:05:25 -0400

    finished styling conditionals page

--- src/components/reactTutorial/tutorialOptions/conditionals/Conditionals.js
+++ src/components/reactTutorial/tutorialOptions/conditionals/Conditionals.js
@@ -10,21 +10,21 @@ const Conditionals = () => {
     return (
         <div className='conditionals__container'>
             <div className='conditionals__content'>
-                <div className='conditinals__section'>
+                <div className='conditionals__section'>
                     <h1>Conditionals</h1>
-                    <p>Components can be conditionally rendered with multiple ways of accomplishing this</p>
+                    <p>Components can be conditionally rendered with multiple ways of accomplishing this.</p>
                 </div>
-                <div className='conditinals__section'>
+                <div className='conditionals__section'>
                     <h2>"if" Statement</h2>
-                    <p>The JavaScript 'if' operator can be used to render Components</p>
+                    <p>The JavaScript 'if' operator can be used to render Components.</p>
                     <img src={If} alt="" />
                 </div>
-                <div className='conditinals__section'>
+                <div className='conditionals__section'>
                     <h2>Logical "&&" Operator</h2>
                     <p>"Another way to conditionally render a React component is by using the && operator."</p>
                     <img src={And} alt="" />
                 </div>
-                <div className='conditinals__section'>
+                <div className='conditionals__section'>
                     <h2>Ternary Operator</h2>
                     <p>The Ternary Operaotr is essentially the "if" operator.</p>
                     <img src={Ternary} alt="" />
