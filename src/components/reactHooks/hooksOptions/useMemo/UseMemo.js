a4bfd9859cec9c59b73147ec90c8bfe8a8d98ef9
matthewAbianco <matthewbianco@hotmail.com>
2022-05-27 09:41:47 -0400

    finished styling useMemo page

--- src/components/reactHooks/hooksOptions/useMemo/UseMemo.js
+++ src/components/reactHooks/hooksOptions/useMemo/UseMemo.js
@@ -7,16 +7,18 @@ const UseMemo = () => {
     const [count, setCount] = useState(0);
     const [input, setInput] = useState('')
 
-    const calculation = useMemo(() => slowFunction(count), [count]);
-    const increment = () => {
-        setCount((c) => c + 1);
-    };
 
     const slowFunction = (num) => {
-        for (let i = 0; i < 1_000_000_000; i++) {
+        for (let i = 1; i < 1_000_000_000; i++) {
             num += 1;
         }
         return num;
+    };
+
+
+    const calculation = useMemo(() => slowFunction(count), [count]);
+    const increment = () => {
+        setCount((c) => c + 1);
     };
 
 
@@ -29,22 +31,23 @@ const UseMemo = () => {
                     <p>While useCallback holds the value of a function, useMemo holds the value of a return statement.</p>
                     <p>The value of useMemo is that it can hold the value of functions that take alot of time to calculate.</p>
                 </div>
-                <div>
+                <div className='umemo__section'>
                     <h2>Working With useMemo</h2>
                     <p>Below are two functions, a slow function and a fast function.</p>
                     <p>Because the slow function is wrapped with useMemo, it will not cause a render when we use our fast function, making the page much faster.</p>
                     <p>The slow function will always be slow, but it will only be slow when its value gets changed.</p>
                 </div>
-                <div>
+                <div className='umemo__section'>
+                    <h2>Simple function</h2>
                     <input type='text' onChange={e => setInput(e.target.value)} value={input} />
                 </div>
-                <div>
+                <div className='umemo__section'>
                     Count: {count}
                     <button onClick={increment}>+</button>
                     <h2>Expensive Calculation</h2>
                     {calculation}
-                </div>
-                <div>
+                    <p></p>
+                    {slowFunction}
                     <img src={UMemo} alt="" />
                 </div>
             </div>
